c7e1455b75e326a8fd13e846401ac82ad12c0ed2
abrahamleyva <abrahamedina24@gmail.com>
2018-04-15 22:33:07 -0700

    updated old database stuff

--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/MainActivity.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/MainActivity.java
@@ -2,21 +2,23 @@ package edu.csumb.abmedina.cst438_project_personal_recipe_finder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MyActivity";
+    DatabaseReference databaseUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseUsers = FirebaseDatabase.getInstance().getReference("users");
+
+        String id = databaseUsers.push().getKey();
+
+        databaseUsers.child(id).setValue("Admin");
     }
 }
