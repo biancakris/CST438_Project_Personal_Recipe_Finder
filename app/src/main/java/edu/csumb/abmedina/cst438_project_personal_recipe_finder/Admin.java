a66614dca2e01e2f71b1a1ab10b22db6d4dc817d
abrahamleyva <abrahamedina24@gmail.com>
2018-04-21 21:41:11 -0700

    list items of all users

--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/Admin.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/Admin.java
@@ -2,12 +2,23 @@ package edu.csumb.abmedina.cst438_project_personal_recipe_finder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Admin extends AppCompatActivity {
+
+    ListView listViewItems;
+
+    List<Item> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,41 @@ public class Admin extends AppCompatActivity {
 
         //addUser(userId,"Abraham", "Standard");
         //addItem(userId, "Chicken", "Meat", 5, "pound");
-        addRestriction(userId, "peanut allergy");
+        //addRestriction(userId, "peanut allergy");
+
+        listViewItems = findViewById(R.id.listViewItems);
+
+        itemList = new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DatabaseReference databaseItems = FirebaseDatabase.getInstance().getReference("items");
+
+        databaseItems.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                itemList.clear();
+
+                for(DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+                    Item item = itemSnapshot.getValue(Item.class);
+
+                    itemList.add(item);
+                }
+
+                ItemList adapter = new ItemList(Admin.this, itemList);
+                listViewItems.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void addUser(String userId, String username, String dietType) {
