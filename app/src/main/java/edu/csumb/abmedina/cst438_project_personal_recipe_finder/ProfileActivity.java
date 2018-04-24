5b193427363d7913addbb4d3e317eb467f999f37
biancakris <bihernandez@csumb.edu>
2018-04-24 16:12:18 -0700

    ui for profile

--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/ProfileActivity.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/ProfileActivity.java
@@ -5,18 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    Spinner dietarySpiner;
     Button adminButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         adminButton = findViewById(R.id.admin_button);
+        dietarySpiner = (Spinner) findViewById(R.id.dietarySpinner);
+
+        //set array adapter for spinner using string array and default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.dietary_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dietarySpiner.setAdapter(adapter);
+
+
 
         adminButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,4 +44,5 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+
 }
