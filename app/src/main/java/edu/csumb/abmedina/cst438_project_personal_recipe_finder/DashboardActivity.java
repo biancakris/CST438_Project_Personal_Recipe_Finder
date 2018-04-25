e37139d4440daa64834348af90e6f5e1d7a89b48
biancakris <bihernandez@csumb.edu>
2018-04-24 18:56:36 -0700

    added intents, and more ui

--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/DashboardActivity.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/DashboardActivity.java
@@ -2,12 +2,34 @@ package edu.csumb.abmedina.cst438_project_personal_recipe_finder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    ImageButton chefButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        chefButton = findViewById(R.id.chefButton);
+
+        //On Click (Save Button)
+        chefButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(DashboardActivity.this, ProfileActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/ProfileActivity.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/ProfileActivity.java
@@ -11,8 +11,10 @@ import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    Spinner dietarySpiner;
+    Spinner dietarySpinner;
+    Spinner allergySpinner;
     Button adminButton;
+    Button saveButton;
 
 
     @Override
@@ -22,15 +24,33 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         adminButton = findViewById(R.id.admin_button);
-        dietarySpiner = (Spinner) findViewById(R.id.dietarySpinner);
+        dietarySpinner = (Spinner) findViewById(R.id.dietarySpinner);
+        allergySpinner = (Spinner) findViewById(R.id.allergySpinner);
+        saveButton = findViewById(R.id.saveButton);
 
         //set array adapter for spinner using string array and default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        ArrayAdapter<CharSequence> dietAdapter = ArrayAdapter.createFromResource(
                 this, R.array.dietary_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dietarySpiner.setAdapter(adapter);
+        dietAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dietarySpinner.setAdapter(dietAdapter);
+
+        //set array adapter for spinner using string array and default spinner layout
+        ArrayAdapter<CharSequence> allergyAdapter = ArrayAdapter.createFromResource(
+                this, R.array.allergy_array, android.R.layout.simple_spinner_item);
+        allergyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        allergySpinner.setAdapter(allergyAdapter);
 
 
+
+        //On Click (Save Button)
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ProfileActivity.this, DashboardActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         adminButton.setOnClickListener(new View.OnClickListener() {
             @Override
