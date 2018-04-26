e035c689f57865b870df0058bb5c054ec8fab3b9
biancakris <bihernandez@csumb.edu>
2018-04-26 00:54:47 -0700

    added chews button to all food lists

--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/FruitsActivity.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/FruitsActivity.java
@@ -2,12 +2,61 @@ package edu.csumb.abmedina.cst438_project_personal_recipe_finder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FruitsActivity extends AppCompatActivity {
+
+    ImageButton chewsButton;
+
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fruits);
+
+        Bundle data = getIntent().getExtras();
+        userId = data.getString("userId");
+
+
+        chewsButton = findViewById(R.id.chewsButton);
+
+        chewsButton = findViewById(R.id.chewsButton);
+        //OnClick(chews)
+        chewsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(FruitsActivity.this, DashboardActivity.class);
+                Bundle data =  new Bundle();
+
+                data.putString("userId", userId);
+                intent.putExtras(data);
+                startActivity(intent);
+
+            }
+        });
     }
 }
