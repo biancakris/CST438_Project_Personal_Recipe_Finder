ed5b4250abfd7c8ded12905a386ec606dd16d0d9
biancakris <bihernandez@csumb.edu>
2018-04-26 00:03:09 -0700

    other food activitys and bg images

--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/FridgeDashboardActivity.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/FridgeDashboardActivity.java
@@ -3,6 +3,7 @@ package edu.csumb.abmedina.cst438_project_personal_recipe_finder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Button;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +12,15 @@ import android.view.View;
 public class FridgeDashboardActivity extends AppCompatActivity {
 
     ImageButton meatButton;
+    ImageButton grainButton;
+    ImageButton veggieButton;
+    ImageButton fruitButton;
+    ImageButton dairyButton;
+    ImageButton otherButton;
     ImageButton chewsButton;
+
+    Button addButton;
+    Button deleteButton;
 
     String userId;
 
@@ -26,6 +35,13 @@ public class FridgeDashboardActivity extends AppCompatActivity {
 
         chewsButton = findViewById(R.id.chewsButton);
         meatButton = findViewById(R.id.meatButton);
+        grainButton = findViewById(R.id.grainsButton);
+        veggieButton = findViewById(R.id.veggieButton);
+        fruitButton = findViewById(R.id.fruitButton);
+        dairyButton = findViewById(R.id.dairyButton);
+        otherButton = findViewById(R.id.otherButton);
+        addButton = findViewById(R.id.addButton);
+        deleteButton = findViewById(R.id.deleteButton);
 
         //OnClick(chews)
         chewsButton.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +62,76 @@ public class FridgeDashboardActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 Intent intent = new Intent(FridgeDashboardActivity.this, MeatActivity.class);
+                Bundle data =  new Bundle();
+
+                data.putString("userId", userId);
+                intent.putExtras(data);
+                startActivity(intent);
+
+            }
+        });
+
+        //Onclick (grains)
+        grainButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(FridgeDashboardActivity.this, GrainsActivity.class);
+                Bundle data =  new Bundle();
+
+                data.putString("userId", userId);
+                intent.putExtras(data);
+                startActivity(intent);
+
+            }
+        });
+
+        //Onclick (Veggies)
+        veggieButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(FridgeDashboardActivity.this, VegetablesActivity.class);
+                Bundle data =  new Bundle();
+
+                data.putString("userId", userId);
+                intent.putExtras(data);
+                startActivity(intent);
+
+            }
+        });
+
+        //OnClick (fruit)
+        fruitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(FridgeDashboardActivity.this, FruitsActivity.class);
+                Bundle data =  new Bundle();
+
+                data.putString("userId", userId);
+                intent.putExtras(data);
+                startActivity(intent);
+
+            }
+        });
+
+        //Onclick (dairy)
+        dairyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(FridgeDashboardActivity.this, DairyActivity.class);
+                Bundle data =  new Bundle();
+
+                data.putString("userId", userId);
+                intent.putExtras(data);
+                startActivity(intent);
+
+            }
+        });
+
+        //OnClick (other)
+        otherButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(FridgeDashboardActivity.this, OtherFoodActivity.class);
                 Bundle data =  new Bundle();
 
                 data.putString("userId", userId);
---
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/GrainsActivity.java
@@ -0,0 +1,13 @@
+package edu.csumb.abmedina.cst438_project_personal_recipe_finder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GrainsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grains);
+    }
+}
