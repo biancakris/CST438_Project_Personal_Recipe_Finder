2034f91ac42d98c3a86264ca5241132b3bfe9203
biancakris <bihernandez@csumb.edu>
2018-04-26 11:16:56 -0700

    added measurement spinner

--- app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/AddItemActivity.java
+++ app/src/main/java/edu/csumb/abmedina/cst438_project_personal_recipe_finder/AddItemActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class AddItemActivity extends AppCompatActivity {
 
     ImageButton chewsButton;
+    Spinner measurementsSpinner;
 
     String userId;
 
@@ -21,6 +24,13 @@ public class AddItemActivity extends AppCompatActivity {
         userId = data.getString("userId");
 
         chewsButton = findViewById(R.id.chewsButton);
+        measurementsSpinner = findViewById(R.id.measurmentSpinner);
+
+        //set array adapter for spinner using string array and default spinner layout
+        ArrayAdapter<CharSequence> measurementAdapter = ArrayAdapter.createFromResource(
+                this, R.array.measurements_array, android.R.layout.simple_spinner_item);
+        measurementAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        measurementsSpinner.setAdapter(measurementAdapter);
 
         chewsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
